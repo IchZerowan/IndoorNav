d98e55b3cb40809fa986bcc4816cc06020793029
Darin <darin.x64@gmail.com>
2021-04-24 10:32:20 +0300

    Added CSVParser and model csv

---
+++ IndoorNav/CSVParser.cs
@@ -0,0 +1,37 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using IndoorNav.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace IndoorNav
+{
+    public class CSVParser
+    {
+        public static List<PointData> ReadCSV (string pathToFile)
+        {
+            List<PointData> data = new List<PointData>();
+
+            using (StreamReader sr = new StreamReader(pathToFile))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    string line = sr.ReadLine();
+                    PointData pd = new PointData();
+                    pd.FirstTimeStamp = line.Split(',')[0];
+                    pd.device_name = line.Split(',')[1];
+                    pd.mac_addr = line.Split(',')[2];
+                    pd.eddystone_instance_id = line.Split(',')[3];
+                    pd.rssi = line.Split(',')[4];
+                    pd.timestamp = line.Split(',')[5];
+
+                    data.Add(pd);
+                }
+            }
+
+            return data;
+        }
+    }
+}
---
+++ IndoorNav/Models/PointData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration.Attributes;
+
+namespace IndoorNav.Models
+{
+    public class PointData
+    {
+        //,device_name,mac_addr,eddystone_instance_id,rssi,timestamp
+
+        [Index(0)]
+        public string FirstTimeStamp { get; set; }
+        [Index(1)]
+        public string device_name { get; set; }
+        [Index(2)]
+        public string mac_addr { get; set; }
+        [Index(3)]
+        public string eddystone_instance_id { get; set; }
+        [Index(4)]
+        public string rssi { get; set; }
+        [Index(5)]
+        public string timestamp { get; set; }
+
+    }
+}
